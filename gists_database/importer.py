78bb0cfce722c298bb531fb23c569b7c130dc1e2
Alex-Skrivseth <alexskrivseth@gmail.com>
2019-06-30 05:04:31 +0000

    Alex Skriveth tests not passing.

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,50 @@
 import requests
+SQL_INSERT_TEMPLATE = ''' INSERT INTO gists (github_id, html_url, git_pull_url, 
+                                            git_push_url, commits_url, forks_url, 
+                                            public, created_at, updated_at, 
+                                            comments, comments_url)
+                                            
+                            VALUES (:github_id, :html_url, :git_pull_url, 
+                                            :git_push_url, :commits_url, :forks_url, 
+                                            :public, :created_at, :updated_at, 
+                                            :comments, :comments_url);
+'''
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    
+    url = 'https://api.github.com/users/{}/gists'.format(username)
+    
+
+    response = requests.get(url)
+    response.raise_for_status()
+   
+    list_of_gists = response.json()
+    # there are nine gists 
+    # each dictionary contains the info for that gist
+    # take the relevant info and put it into the db.
+    for gist in list_of_gists:
+
+        params = {
+            'github_id':gist['id'],
+            'html_url':gist['html_url'],
+            'git_pull_url':gist['git_pull_url'],
+            'git_push_url':gist['git_push_url'],
+            'commits_url':gist['commits_url'],
+            'forks_url':gist['forks_url'],
+            'public':gist['public'],
+            'created_at':gist['created_at'],
+            'updated_at':gist['updated_at'],
+            'comments':gist['comments'],
+            'comments_url':gist['comments_url']
+
+            
+        }
+        
+        db.execute(SQL_INSERT_TEMPLATE, params)
+        
+    if commit:
+        db.commit()
+        
+    
+
+    
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,47 @@
 from .models import Gist
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    results = []
+    c = db_connection.cursor()
+    github_id = None
+    created_at = None
+    
+    if kwargs:
+        for key,value in kwargs.items():
+            if key == "github_id":
+                github_id = value
+            if key == "created_at":
+                created_at = value
+    
+    if github_id:
+        c.execute("SELECT * FROM gists WHERE github_id = :github_id",
+                                                        {"github_id":github_id})
+        list_of_tuples = c.fetchall()
+        for gist in list_of_tuples:
+            g = Gist(gist)
+            results.append(g)
+        print(len(results))
+        return results
+    if created_at:
+        c.execute("SELECT * FROM gists WHERE datetime(created_at) = datetime(:created_at)",
+                                                        {"created_at":created_at})
+        list_of_tuples = c.fetchall()
+        for gist in list_of_tuples:
+            g = Gist(gist)
+            results.append(g)
+        print(len(results))
+        return results
+    
+    
+    c.execute("SELECT * FROM gists")
+    list_of_tuples = c.fetchall()
+
+    for gist in list_of_tuples:
+        g = Gist(gist)
+        results.append(g)
+    print(len(results))
+    return results
+        
+            
+    
